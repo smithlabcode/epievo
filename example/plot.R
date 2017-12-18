d445789c5d73556fa3ace7b02c58ecfd0cf3d8f1
jqujqu <jqu@usc.edu>
2017-12-18 15:19:36 -0800

    Update example scripts

--- example/plot.R
+++ example/plot.R
@@ -12,11 +12,12 @@ if (length(args)<4) {
 
 
 x <- read.table(paths, as.is=T)
+tot <- as.numeric(unlist(strsplit(x$V3[1], ",")))[2]
 
 pdf (pathplot, width=120, height=3, pointsize=8 )
 colors <- c("cadetblue", "black")
-plot(x=c(0,nrow(x)), y=c(0,5000), pch="", xlab= "Position", ylab="Time")
-rect(xleft=0, ybottom=0, xright=nrow(x), ytop=5000)
+plot(x=c(0,nrow(x)), y=c(0,tot), pch="", xlab= "Position", ylab="Time")
+rect(xleft=0, ybottom=0, xright=nrow(x), ytop=tot)
 for (i in 1:nrow(x)) {
 xval <- x$V1[i]
 s <- x$V2[i]
@@ -32,11 +33,11 @@ for (k in 1:(length(yvals)-1)) {
 dev.off()
 
 
-y <- read.table(freqs, skip=6)
+y <- read.table(freqs)
 pdf(freqplot, width=4, height=4, pointsize=8)
 par(mfrow=c(2,2), mar=c(5,4,1,1))
-plot(y$V1, xlab="Iteration", ylab="Freq 00")
-plot(y$V4, xlab="Iteration", ylab="Freq 11")
-plot(y$V2, xlab="Iteration", ylab="Freq 01")
-plot(y$V3, xlab="Iteration", ylab="Freq 10")
+plot(y$V2, y$V3, xlab="Time", ylab="Freq 00")
+plot(y$V2, y$V6, xlab="Time", ylab="Freq 11")
+plot(y$V2, y$V4, xlab="Time", ylab="Freq 01")
+plot(y$V2, y$V5, xlab="Time", ylab="Freq 10")
 dev.off()
--- example/run.sh
+++ example/run.sh
@@ -1,3 +1,4 @@
-evosim test.param -o test.paths &> test.out
-awk 'NR>6{print}' test.out.tmp
-Rscript plot.R test.paths test_evo_path.pdf test.out test_freq_trace.pdf
+prefix=test
+evosim ${prefix}.param -v -o ${prefix}_path.txt 1> ${prefix}_freq.txt 
+Rscript plot.R ${prefix}_path.txt ${prefix}_path.pdf ${prefix}_freq.txt  ${prefix}_freq.pdf 
+
