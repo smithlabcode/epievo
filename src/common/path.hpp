3e5ad315a4b97ff568c10d9081a025a0b289ed28
jqujqu <jqu@usc.edu>
2018-01-22 17:38:33 -0800

    Add function to read paths from file

--- src/common/path.hpp
+++ src/common/path.hpp
@@ -3,16 +3,19 @@
 
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
 
 using std::vector;
+using std::string;
 
-struct path {
+struct Path {
   bool init_state;
   double tot_time;
   vector<double> jumps;
 };
 
-struct environment {
+struct Environment {
   vector<bool> left;
   vector<bool> right;
   vector<double> breaks;
@@ -20,7 +23,7 @@ struct environment {
 };
 
 void initialize_paths(const std::vector<bool> &seq, const double tot_time,
-                        std::vector<path> &paths) {
+                      std::vector<Path> &paths) {
   paths.resize(seq.size());
   for (size_t i = 0; i < seq.size(); ++i) {
     paths[i].init_state = seq[i];
@@ -29,7 +32,7 @@ void initialize_paths(const std::vector<bool> &seq, const double tot_time,
   }
 }
 
-void intersect_paths(const path &pa, const path &pb, environment &env) {
+void intersect_paths(const Path &pa, const Path &pb, Environment &env) {
   assert(pa.tot_time == pb.tot_time);
   bool sa = pa.init_state;
   bool sb = pb.init_state;
@@ -73,7 +76,7 @@ void intersect_paths(const path &pa, const path &pb, environment &env) {
   }
 }
 
-void end_sequence(const vector<path> &paths,
+void end_sequence(const vector<Path> &paths,
                   vector<bool> &seq) {
   seq.resize(paths.size());
   for (size_t i = 0; i < paths.size(); ++i) {
@@ -82,14 +85,14 @@ void end_sequence(const vector<path> &paths,
   }
 }
 
-bool state_at_time(const path &p, const double t) {
+bool state_at_time(const Path &p, const double t) {
   bool s = p.init_state;
   for (size_t i = 0; i < p.jumps.size() && p.jumps[i] < t; ++i)
     s = !s;
   return s;
 }
 
-void sequence_at_time(const vector<path> &paths,
+void sequence_at_time(const vector<Path> &paths,
                       const double t,
                       vector<bool> &seq) {
   seq.resize(paths.size());
@@ -97,5 +100,47 @@ void sequence_at_time(const vector<path> &paths,
     seq[i] = state_at_time(paths[i], t);
   }
 }
+
+void to_path(const bool s, const string jumps, Path &p) {
+  p.init_state = s;
+  vector<string> fields = smithlab::split(jumps, ",", false);
+  p.tot_time = std::stod(fields.back());
+  p.jumps.clear();
+  for (size_t i = 1; i < fields.size()-1; ++i)
+    p.jumps.push_back(std::stod(fields[i]));
+}
+
+void read_paths(const string path_file, vector<vector<Path> > &paths) {
+  std::ifstream in(path_file.c_str());
+  if (!in)
+    throw SMITHLABException("cannot read: " + path_file);
+
+  vector<size_t> node_ids;
+  size_t node_id, pos;
+  bool init_state;
+  string jumpstring;
+  string line;
+  while (std::getline(in, line)) {
+    if (line[0]!= '#') {
+      std::istringstream iss(line);
+      iss >> node_id >> pos >> init_state >> jumpstring;
+      if (node_ids.size() == 0 || node_id != node_ids.back()) {
+        node_ids.push_back(node_id);
+        vector<Path> pp;
+        paths.push_back(pp);
+      }
+      Path p;
+      to_path(init_state, jumpstring, p);
+      paths[node_ids.size() - 1].push_back(p);
+    }
+  }
+}
+
+void get_inital_seq(const vector<Path> paths, vector<bool> &seq) {
+  seq.clear();
+  for (size_t i = 0; i < paths.size(); ++i)
+    seq.push_back(paths[i].init_state);
+}
+
 
 #endif
