fc00d2292a2703a39046236e53cb835e38b891f9
andrewdavidsmith <andrew@smithlabresearch.org>
2018-02-25 14:20:59 -0800

    Adding a cpp file to move code out of the header

--- src/common/param.hpp
+++ src/common/param.hpp
@@ -3,68 +3,19 @@
 
 #include <string>
 #include <vector>
-#include <fstream>
-#include <sstream>
 
-using std::vector;
-using std::string;
-
+#include "PhyloTreePreorder.hpp"
 
 struct model_param {
   PhyloTreePreorder t;
   size_t n_site;
-  vector<vector<double> > stationary_logfac; // 2 by 2
-  vector<vector<double> > stationary_logbaseline;  // 2 by 2
-  vector<vector<double> > init_logfac;
+  std::vector<std::vector<double> > stationary_logfac; // 2 by 2
+  std::vector<std::vector<double> > stationary_logbaseline;  // 2 by 2
+  std::vector<std::vector<double> > init_logfac;
 
-  void read_param(const string param_file);
-  void get_rates(vector<double> &rates) const;
+  void read_param(const std::string &param_file);
+  void get_rates(std::vector<double> &rates) const;
 };
 
-
-void model_param::read_param(const string param_file) {
-  std::ifstream in(param_file.c_str());
-  if (!in)
-    throw std::runtime_error("Could not open file" + param_file);
-
-  string dummy_label;
-  in >> dummy_label >> t;
-  in >> dummy_label >> n_site;
-
-  stationary_logfac =
-    vector<vector<double> >(2, vector<double>(2, 0.0));
-  stationary_logbaseline =
-    vector<vector<double> >(2, vector<double>(2, 0.0));
-  init_logfac =
-    vector<vector<double> >(2, vector<double>(2, 0.0));
-
-  in >> dummy_label >> stationary_logfac[0][0]
-     >> stationary_logfac[0][1] >> stationary_logfac[1][1];
-  assert(dummy_label == "stationary");
-  stationary_logfac[1][0] = stationary_logfac[0][1];
-
-  in >> dummy_label >> stationary_logbaseline[0][0]
-     >> stationary_logbaseline[0][1] >> stationary_logbaseline[1][1];
-  assert(dummy_label == "baseline");
-  stationary_logbaseline[1][0] = stationary_logbaseline[0][1];
-
-  in >> dummy_label >> init_logfac[0][0]
-     >> init_logfac[0][1] >> init_logfac[1][1];
-  assert(dummy_label == "init");
-  init_logfac[1][0] = init_logfac[0][1];
-}
-
-void model_param::get_rates(vector<double> &rates) const {
-  rates.resize(8, 0);
-  for (size_t i = 0; i < 2; ++i) {
-    for (size_t j = 0; j < 2; ++j) {
-      for (size_t k = 0; k < 2; ++k) {
-        rates[4*i+2*j+k] = exp(stationary_logfac[i][1-j] +
-                               stationary_logfac[1-j][k] +
-                               stationary_logbaseline[i][k]);
-      }
-    }
-  }
-}
 
 #endif
