ea374560dae43be9850ce75b4c4392bb88efad3e
jqujqu <jqu@usc.edu>
2018-03-01 19:29:55 -0800

    Add TriplePath structure

--- src/common/path.cpp
+++ src/common/path.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <sstream>
 #include <cassert>
+#include <algorithm>    // std::lower_bound, std::upper_bound, std::sort
 
 #include "smithlab_utils.hpp"
 
@@ -21,50 +22,6 @@ void initialize_paths(const std::vector<bool> &seq, const double tot_time,
   }
 }
 
-void intersect_paths(const Path &pa, const Path &pb, Environment &env) {
-  assert(pa.tot_time == pb.tot_time);
-  bool sa = pa.init_state;
-  bool sb = pb.init_state;
-  size_t i = 0;
-  size_t j = 0;
-  env.tot_time = pa.tot_time;
-  while (i < pa.jumps.size() || j < pb.jumps.size()) {
-    env.left.push_back(sa);
-    env.right.push_back(sb);
-    if (i < pa.jumps.size() && j < pb.jumps.size()) {
-      if (pa.jumps[i] < pb.jumps[j]) {
-        env.breaks.push_back(pa.jumps[i]);
-        ++i;
-        sa = !sa;
-      } else if (pa.jumps[i] > pb.jumps[j]) {
-        env.breaks.push_back(pb.jumps[j]);
-        ++j;
-        sb = !sb;
-      } else {
-        env.breaks.push_back(pb.jumps[j]);
-        ++j;
-        ++i;
-        sa = !sa;
-        sb = !sb;
-      }
-    } else if (i < pa.jumps.size()) {
-      env.breaks.push_back(pa.jumps[i]);
-      ++i;
-      sa = !sa;
-    } else {
-      env.breaks.push_back(pb.jumps[j]);
-      ++j;
-      sb = !sb;
-    }
-  }
-
-  if (env.breaks.size() == 0 ||
-      env.breaks.back() < env.tot_time) {
-    env.left.push_back(sa);
-    env.right.push_back(sb);
-  }
-}
-
 void end_sequence(const vector<Path> &paths,
                   vector<bool> &seq) {
   seq.resize(paths.size());
@@ -75,14 +32,17 @@ void end_sequence(const vector<Path> &paths,
 }
 
 bool state_at_time(const Path &p, const double t) {
-  bool s = p.init_state;
-  for (size_t i = 0; i < p.jumps.size() && p.jumps[i] < t; ++i)
-    s = !s;
+  vector<double>::const_iterator low;
+  low = std::lower_bound(p.jumps.begin(), p.jumps.end(), t);
+  size_t idx = (size_t)(low- p.jumps.begin());
+  bool s = (idx%2 == 0)?  p.init_state: !p.init_state;
+  // bool s = p.init_state;
+  // for (size_t i = 0; i < p.jumps.size() && p.jumps[i] < t; ++i)
+  //   s = !s;
   return s;
 }
 
-void sequence_at_time(const vector<Path> &paths,
-                      const double t,
+void sequence_at_time(const vector<Path> &paths, const double t,
                       vector<bool> &seq) {
   seq.resize(paths.size());
   for (size_t i = 0; i < paths.size(); ++i) {
@@ -129,4 +89,92 @@ void get_inital_seq(const vector<Path> paths, vector<bool> &seq) {
   seq.clear();
   for (size_t i = 0; i < paths.size(); ++i)
     seq.push_back(paths[i].init_state);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void intersect_paths(const Path &pa, const Path &pb, Environment &env) {
+  assert(pa.tot_time == pb.tot_time);
+  bool sa = pa.init_state;
+  bool sb = pb.init_state;
+  size_t i = 0;
+  size_t j = 0;
+  env.tot_time = pa.tot_time;
+  while (i < pa.jumps.size() || j < pb.jumps.size()) {
+    env.left.push_back(sa);
+    env.right.push_back(sb);
+    if (i < pa.jumps.size() && j < pb.jumps.size()) {
+      if (pa.jumps[i] < pb.jumps[j]) {
+        env.breaks.push_back(pa.jumps[i]);
+        ++i;
+        sa = !sa;
+      } else if (pa.jumps[i] > pb.jumps[j]) {
+        env.breaks.push_back(pb.jumps[j]);
+        ++j;
+        sb = !sb;
+      } else {
+        env.breaks.push_back(pb.jumps[j]);
+        ++j;
+        ++i;
+        sa = !sa;
+        sb = !sb;
+      }
+    } else if (i < pa.jumps.size()) {
+      env.breaks.push_back(pa.jumps[i]);
+      ++i;
+      sa = !sa;
+    } else {
+      env.breaks.push_back(pb.jumps[j]);
+      ++j;
+      sb = !sb;
+    }
+  }
+
+  if (env.breaks.size() == 0 ||
+      env.breaks.back() < env.tot_time) {
+    env.left.push_back(sa);
+    env.right.push_back(sb);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+TriplePath::TriplePath(const Path &l, const Path &m, const Path &r) {
+  assert(l.tot_time == m.tot_time && l.tot_time == r.tot_time);
+  size_t sl = (size_t)(l.init_state);
+  size_t sm = (size_t)(m.init_state);
+  size_t sr = (size_t)(r.init_state);
+  states.clear();
+  states.push_back(sl * 4 + sm * 2 + sr);
+
+  breaks.clear();
+  breaks.insert(breaks.end(), l.jumps.begin(), l.jumps.end());
+  breaks.insert(breaks.end(), m.jumps.begin(), m.jumps.end());
+  breaks.insert(breaks.end(), r.jumps.begin(), r.jumps.end());
+  std::sort(breaks.begin(), breaks.end());
+  breaks.push_back(l.tot_time);
+
+  for (size_t i = 1; i < breaks.size(); ++i) {
+    const double t = breaks[i-1] + (breaks[i] - breaks[i-1])/2;
+    const size_t state_l = (size_t)(state_at_time(l, t));
+    const size_t state_m = (size_t)(state_at_time(m, t));
+    const size_t state_r = (size_t)(state_at_time(r, t));
+    states.push_back(state_l * 4 + state_m * 2 + state_r);
+  }
+
+  jump_context_freq.resize(8, 0);
+  for (size_t i = 0; i < m.jumps.size(); ++i) {
+    vector<double>::iterator low;
+    low = std::lower_bound (breaks.begin(), breaks.end(), m.jumps[i]);
+    const size_t context = states[(size_t)(low - breaks.begin())];
+    ++jump_context_freq[context];
+  }
+}
+
+void
+TriplePath::time_by_context(vector<double> &tbc) const {
+  tbc.resize(8, 0.0);
+  tbc[states[0]] += breaks[0];
+  for (size_t i = 0; i < states.size(); ++i)
+    tbc[states[i]] += breaks[i+1] - breaks[i];
 }
