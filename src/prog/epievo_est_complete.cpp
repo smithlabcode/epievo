d96b0283042bc4bcd8f806612cf8a52e9c5c8b93
andrewdavidsmith <andrew@smithlabresearch.org>
2018-03-11 17:22:03 -0700

    Major changes to produce a version that can run without crashing

--- src/prog/epievo_est_complete.cpp
+++ src/prog/epievo_est_complete.cpp
@@ -23,20 +23,18 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
-#include <random>
-#include <math.h>   /* exp, sqrt, pow fabs*/
+#include <cmath>   /* exp, sqrt, pow fabs*/
 #include <numeric>  /* std::inner_product, accumulate*/
 #include <functional>
 
 #include "OptionParser.hpp"
 #include "smithlab_utils.hpp"
 #include "smithlab_os.hpp"
+
 #include "PhyloTreePreorder.hpp"
-#include "Path.hpp"  /* related to Path */
-#include "TwoStateSeq.hpp"
-#include "param.hpp" /* model_param */
-#include "TripletPattern.hpp"
-#include "jump.hpp"
+#include "Path.hpp"
+#include "StateSeq.hpp"
+#include "EpiEvoModel.hpp"
 
 using std::vector;
 using std::endl;
@@ -44,17 +42,50 @@ using std::cerr;
 using std::cout;
 using std::string;
 
+
+/* add the total time for each branch to the set of jump times at each
+   site for each branch. This allows more convenient processing of
+   jump sequences, but should be undone for cases when the final jump
+   would imply a state change */
+static void
+add_total_time_to_jumps(vector<vector<Path> > &all_paths) {
+  for (size_t i = 0; i < all_paths.size(); ++i)
+    if (!all_paths[i].empty()) {
+      // make sure all time durations are the same for each site
+      assert(all_paths[i].front().tot_time ==
+             all_paths[i].back().tot_time);
+      const double tot_time = all_paths[i].front().tot_time;
+      for (size_t j = 0; j < all_paths[i].size(); ++j)
+        all_paths[i][j].jumps.push_back(tot_time);
+    }
+}
+
+/* undo the changes made by "add_total_time_to_jumps" */
+static void
+remove_total_time_from_jumps(vector<vector<Path> > &all_paths) {
+  for (size_t i = 0; i < all_paths.size(); ++i)
+    if (!all_paths[i].empty()) {
+      // make sure we are removing something added
+      assert(all_paths[i].front().jumps.back() ==
+             all_paths[i].back().jumps.back());
+      for (size_t j = 0; j < all_paths[i].size(); ++j)
+        all_paths[i][j].jumps.pop_back();
+    }
+}
+
 void
 add_sufficient_statistics(const Path &left, const Path &mid, const Path &right,
                           vector<double> &J, vector<double> &D) {
 
-  assert(left.tot_time == mid.tot_time && mid.tot_time == right.tot_time);
+  // make sure the total time was put at the end of all jump sequences
+  assert(left.jumps.back() == mid.jumps.back() &&
+         left.jumps.back() == right.jumps.back());
 
   size_t triplet = triple2idx(left.init_state, mid.init_state, right.init_state);
   double prev_time = 0.0;
   size_t i = 0, j = 0, k = 0;
   while (i < left.jumps.size() && j < mid.jumps.size() && k < right.jumps.size())
-    if (left.jumps[i] < min(mid.jumps[j], right.jumps[k])) { // LEFT
+    if (left.jumps[i] < std::min(mid.jumps[j], right.jumps[k])) { // LEFT
       D[triplet] += left.jumps[i] - prev_time;
       /* no need to update J[triplet] += 1.0; here */
       prev_time = left.jumps[i];
@@ -68,7 +99,7 @@ add_sufficient_statistics(const Path &left, const Path &mid, const Path &right,
       triplet = flip_mid_bit(triplet);
       ++j;
     }
-    else { // RIGHT
+    else { // RIGHT (also: default case, shouldn't udpate J)
       D[triplet] += right.jumps[k] - prev_time;
       /* no need to update J[triplet] += 1.0; here */
       prev_time = right.jumps[k];
@@ -77,23 +108,18 @@ add_sufficient_statistics(const Path &left, const Path &mid, const Path &right,
     }
 }
 
-
 void
 add_sufficient_statistics(const vector<Path> &paths,
                           vector<double> &J, vector<double> &D) {
-
-  for (size_t i = 0; i < n_sites; ++i)
-    paths[i].jumps.push_back(paths[i].tot_time);
-
+  // iterate over sites with valid triples (i.e. not the first and last)
+  const size_t n_sites = paths.size();
+  assert(n_sites > 0); // make sure the root isn't processed
   for (size_t i = 1; i < n_sites - 1; ++i)
     add_sufficient_statistics(paths[i-1], paths[i], paths[i+1], J, D);
-
-  for (size_t i = 0; i < n_sites; ++i)
-    paths[i].jumps.pop_back();
 }
 
 void
-get_sufficient_statistics(const vector<vector<Path> > &the_paths,
+get_sufficient_statistics(const vector<vector<Path> > &all_paths,
                           vector<double> &J, vector<double> &D) {
 
   static const size_t n_triplets = 8;
@@ -101,33 +127,11 @@ get_sufficient_statistics(const vector<vector<Path> > &the_paths,
   J.resize(n_triplets, 0.0);
   D.resize(n_triplets, 0.0);
 
-  for (size_t i = 0; i < the_paths.size(); ++i)
-    add_sufficient_statistics(the_paths[i], J, D);
+  // iterate over nodes, starting at 1 to avoid root
+  for (size_t i = 1; i < all_paths.size(); ++i)
+    add_sufficient_statistics(all_paths[i], J, D);
 }
 
-
-string
-format_two_by_two(const vector<vector<double> > &m) {
-  std::ostringstream oss;
-  oss << "["
-      << std::setw(10) << std::right << m[0][0]
-      << std::setw(10) << std::right << m[0][1]
-      << "]\n["
-      << std::setw(10) << std::right << m[1][0]
-      << std::setw(10) << std::right << m[1][1]
-      << "]";
-  return oss.str();
-}
-
-// Steps:
-// read in full paths
-// extract initial seq, jumps
-// sort jumps
-// derive jump contexts
-
-////////////////////////////////////////////////////////////////////////////////
-// Estimate rates given branch lenghts and jumping times
-////////////////////////////////////////////////////////////////////////////////
 
 static double
 log_likelihood(const vector<double> &J, const vector<double> &D,
@@ -144,6 +148,7 @@ log_likelihood(const vector<double> &J, const vector<double> &D,
 
   return l;
 }
+
 
 /* compute gradients wrt log(rate[i]) */
 void
@@ -186,6 +191,7 @@ get_gradient(const vector<double> &J, const vector<double> &D,
   /* gradient[7] remains 0, since rates[7] is determined by other rates */
 }
 
+
 static void
 candidate_rates(const double step,
                 const vector<double> &rates,
@@ -207,87 +213,62 @@ candidate_rates(const double step,
         - 2*log(updated_rates[1])); // 001 -> 011 (twice,denominator)
 }
 
+
+/* makes a single step of gradient ascent; identifies a new set of
+   rates using the summary statistics in J and D */
 double
 gradient_ascent(const double param_tol,
-                const vector<double> &J,
-                const vector<double> &D,
-                const vector<double> &rates,
-                vector<double> &updated_rates) {
+                const vector<double> &J, const vector<double> &D,
+                const vector<double> &rates, vector<double> &updated_rates) {
+
   /* compute llk and gradient */
-  double l = log_likelihood(J, D, rates);
+  double llk = log_likelihood(J, D, rates);
+  vector<double> gradient;
+  get_gradient(J, D, rates, gradient);
 
-  vector<double> g;
-  get_gradient(J, D, rates, g);
-
+  /* get the step size */
   double norm = 0.0;
-  for (size_t i = 0; i < g.size(); ++i)
-    norm += fabs(g[i]);
+  for (size_t i = 0; i < gradient.size(); ++i)
+    norm += fabs(gradient[i]);
+  double step_size = 0.2/norm; // MAGIC!!!!
 
-  double step = 0.2/norm; // MAGIC!!!!
-  double new_l = 0;
+  double updated_llk = 0;
   do {
-    step *= 0.5;
-    candidate_rates(step, rates, g, updated_rates);
-    new_l = log_likelihood(J, D, updated_rates);
-  }
-  while (new_l <= l && step > param_tol);
-
-  return step;
-}
-
-void
-estimate_rates(const double param_tol,
-               const vector<vector<Path> > &the_paths,
-               const vector<double> &rates,
-               vector<double> &updated_rates) {
-
-  vector<double> J;
-  vector<double> D;
-  get_sufficient_statistics(the_paths, J, D);
-
-  vector<double> current_rates(rates);
-  double current_llk = log_likelihood(J, D, current_rates);
-
-  double diff = gradient_ascent(param_tol, J, D, current_rates, updated_rates);
-  double updated_llk = log_likelihood(J, D, updated_rates);
-
-  while (updated_llk - current_llk > param_tol && diff > param_tol) {
-    current_llk = updated_llk;
-    current_rates = updated_rates;
-    diff = gradient_ascent(param_tol, J, D, current_rates, updated_rates);
+    step_size *= 0.5;
+    candidate_rates(step_size, rates, gradient, updated_rates);
     updated_llk = log_likelihood(J, D, updated_rates);
   }
+  while (updated_llk <= llk && step_size > param_tol);
+
+  return step_size;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
 
-static void
-estimate_transition_probabilities(const vector<bool> &seq,
-                                  vector<vector<double> > & trans_prob) {
+double
+estimate_rates(const double param_tol,
+               const vector<double> &J, const vector<double> &D,
+               const vector<double> &input_rates,
+               vector<double> &rates) {
 
-  // count the different pair-wise transitions within the sequence
-  double c00 = 0.0, c01 = 0.0, c10 = 0.0, c11 = 0.0;
-  for (size_t i = 0; i < seq.size() - 1; ++i) {
-    c00 += static_cast<size_t>((seq[i] == false) && (seq[i+1] == false));
-    c01 += static_cast<size_t>((seq[i] == false) && (seq[i+1] == true));
-    c10 += static_cast<size_t>((seq[i] == true) && (seq[i+1] == false));
-    c11 += static_cast<size_t>((seq[i] == true) && (seq[i+1] == true));
+  rates = input_rates;
+  double llk = log_likelihood(J, D, rates);
+
+  bool improving = true;
+  while (improving) {
+
+    vector<double> updated_rates(rates);
+    const double diff = gradient_ascent(param_tol, J, D, rates, updated_rates);
+    const double updated_llk = log_likelihood(J, D, updated_rates);
+
+    improving = (updated_llk - llk > param_tol && diff > param_tol);
+    if (improving) {
+      llk = updated_llk;
+      rates = updated_rates;
+    }
   }
-  if (c00*c11*c10*c01 == 0)
-    cerr << "WARNING: Root sequence lack diversity" << endl;
-
-  // calculate horizontal probability of no change
-  const double p00 = c00/(c00 + c01);
-  const double p11 = c11/(c10 + c11);
-
-  // assign horizontal probabilities to matrix
-  trans_prob = vector<vector<double> >(2, vector<double>(2, 0.0));
-  trans_prob[0][0] = p00;
-  trans_prob[0][1] = 1.0 - p00;
-  trans_prob[1][0] = 1.0 - p11;
-  trans_prob[1][1] = p11;
+  return llk;
 }
+
 
 /* This function scales the branches of the tree so that the sum of
  * the branch lengths is 1.
@@ -308,18 +289,39 @@ scale_treesize(vector<double> &rates, vector<double> &branches) {
 }
 
 
+static void
+scale_by_rate_factor(vector<double> &rates, vector<double> &branches,
+                     vector<double> &D) {
+
+  /* scale rates and branches to have unit branch length
+     corresponding to one expected transition per site */
+  const double the_rate_factor = rate_scaling_factor(rates);
+
+  // scale the rates and summary stats for each triplet
+  for (size_t triplet_id = 0; triplet_id < rates.size(); ++triplet_id) {
+    rates[triplet_id] /= the_rate_factor;
+    D[triplet_id] *= the_rate_factor;
+  }
+
+  // scale the length of the branch associated with each node
+  for (size_t node_id = 0; node_id < branches.size(); ++node_id)
+    branches[node_id] *= the_rate_factor;
+}
+
+
 int main(int argc, const char **argv) {
 
   try {
 
+    static const double param_tol = 1e-10;
+
     string outfile;
-    string param_file;
     bool VERBOSE = false;
 
+    ////////////////////////////////////////////////////////////////////////
     OptionParser opt_parse(strip_path(argv[0]), "estimate parameters from"
                            " complete data (site-specific paths)",
-                           "<path-file>");
-    opt_parse.add_opt("param", 'p', "params file", false, param_file);
+                           "<path-file> <init-param-file>");
     opt_parse.add_opt("verbose", 'v', "print more run info",
                       false, VERBOSE);
     vector<string> leftover_args;
@@ -337,137 +339,100 @@ int main(int argc, const char **argv) {
       cerr << opt_parse.option_missing_message() << endl;
       return EXIT_SUCCESS;
     }
-    if (leftover_args.size() < 1) {
+    if (leftover_args.size() != 2) {
       cerr << opt_parse.help_message() << endl;
       return EXIT_SUCCESS;
     }
-    const string path_file(leftover_args.back());
+    const string path_file(leftover_args.front());
+    const string init_model_file(leftover_args.back());
     ////////////////////////////////////////////////////////////////////////
 
     if (VERBOSE)
       cerr << "[READING PATHS: " << path_file << "]" << endl;
-    vector<vector<Path> > paths; // along multiple branches
+    vector<vector<Path> > all_paths; // along multiple branches
     vector<string> node_names;
-    read_paths(path_file, node_names, paths);
+    read_paths(path_file, node_names, all_paths);
+
+    /* adding the total time to all jump sequences for convenience,
+       but might need to remove later */
+    add_total_time_to_jumps(all_paths);
+
     const size_t n_nodes = node_names.size();
-    const size_t n_sites = paths.front().size();
+    /* below: 1st element of all_paths empty at root; use last */
+    const size_t n_sites = all_paths.back().size();
 
     if (VERBOSE)
       cerr << "n_nodes=" << n_nodes << endl
            << "n_sites=" << n_sites << endl;
 
-    vector<double> branches_from_paths;
-    for (size_t i = 0; i < paths.size(); ++i)
-      branches_from_paths.push_back(paths[i][0].tot_time);
+    if (VERBOSE)
+      cerr << "[COMPUTING INITIAL SUFFICIENT STATISTICS]" << endl;
 
-    vector<bool> seq;
-    get_initial_seq(paths[0], seq);
-
-    vector<vector<double> > init_trans_prob;
-    estimate_transition_probabilities(seq, init_trans_prob);
+    // get initial values of sufficient statistics
+    vector<double> J;
+    vector<double> D;
+    get_sufficient_statistics(all_paths, J, D);
 
     if (VERBOSE)
-      cerr << "horizontal transitions at root" << endl
-           << format_two_by_two(init_trans_prob) << endl;
+      cerr << "J:" << endl
+           << triplet_info_to_string(J) << endl
+           << "D:" << endl
+           << triplet_info_to_string(D) << endl;
 
-    vector<vector<Jump> > jumps;
-    vector<Hold> holds;
+    // get branch lengths implicit in the path information
+    vector<double> branches_from_paths(all_paths.size(), 0.0);
+    for (size_t i = 0; i < all_paths.size(); ++i)
+      if (!all_paths[i].empty())
+        branches_from_paths[i] = all_paths[i][0].tot_time;
 
-    /* COLLECT ALL JUMPS */
-    for (size_t node_id = 1; node_id < n_nodes; ++node_id) {
+    if (VERBOSE)
+      cerr << "reading parameter file: " << init_model_file << endl;
+    EpiEvoModel the_model;
+    read_model(init_model_file, the_model);
+    if (VERBOSE)
+      cerr << the_model << endl;
 
-      vector<Jump> j;
-      Hold h;
-      get_jumps(paths[node_id], j, h);
-      jumps.push_back(j);
-      holds.push_back(h);
+    if (VERBOSE)
+      cerr << "initial log-likelihood: "
+           << log_likelihood(J, D, the_model.triplet_rates) << endl;
 
-      if (VERBOSE) {
-        cerr << "[PROCESSING BRANCH: " << node_names[node_id] << ", "
-             << "SITES: " << n_sites << ", "
-             << "JUMPS: " << jumps[node_id].size() << endl;
-      }
-    }
+    vector<double> gradient;
+    get_gradient(J, D, the_model.triplet_rates, gradient);
+    if (VERBOSE)
+      cerr << "gradient:" << endl
+           << triplet_info_to_string(gradient) << endl;
 
-    /* IF MODEL PARAMETERS ARE PROVIDED, READ THEM */
-    if (!param_file.empty()) {
-      model_param p;
-      p.read_param(param_file);
-      vector<double> J;
-      vector<double> D;
-      get_sufficient_statistics(the_paths, J, D);
+    vector<double> updated_rates;
+    estimate_rates(param_tol, J, D, the_model.triplet_rates, updated_rates);
+    if (VERBOSE)
+      cerr << "updated rates:" << endl
+           << triplet_info_to_string(updated_rates) << endl;
 
-      vector<double> rates;
-      p.get_rates(rates);
+    if (VERBOSE)
+      cerr << "updated log-likelihood: "
+           << log_likelihood(J, D, updated_rates) << endl;
 
-      vector<double> branches;
-      p.t.get_branch_lengths(branches);
-      branches.erase(branches.begin());
+    /* scale rates and branches to have unit branch length
+       corresponding to one expected transition per site */
+    vector<double> updated_branches(the_model.branches);
+    scale_by_rate_factor(updated_rates, updated_branches, D);
+    // scale_treesize(updated_rates, updated_branches);
 
-      if (VERBOSE) {
-        cerr << "Provided starting rates:\t";
-        for (size_t i = 0; i < rates.size(); ++i) cerr << rates[i] << "\t";
-        cerr << endl;
-        cerr << "Provided starting branches:\t";
-        for (size_t i = 0; i < branches.size(); ++i) cerr << branches[i] << "\t";
-        cerr << endl;
-        cerr << "log-likelihood= "
-             << log_likelihood(J, D, rates) << endl;
-      }
+    if (VERBOSE) {
+      cerr << "scaled branches:\t" << endl;
+      for (size_t i = 0; i < node_names.size(); ++i)
+        cerr << node_names[i] << '\t' << updated_branches[i] << endl;
 
-      vector<double> gradient;
-      get_gradient(J, D, rates, gradient);
+      cerr << "scaled rates:\t" << endl
+           << triplet_info_to_string(updated_rates) << endl;
 
-      if (DEBUG) {
-        cerr << "gradient :" << endl;
-        for (size_t i = 0; i < gradient.size(); ++i)
-          cerr << "[" << i << "]\t" << gradient[i] << endl;
-      }
+      cerr << "scaled D:" << endl
+           << triplet_info_to_string(D) << endl;
 
-      double param_tol = 1e-10;
-      vector<double> updated_rates;
-      estimate_rates(param_tol, the_paths, rates, updated_rates);
-
-      if (VERBOSE) {
-        cerr << "optimized rates:\t" << endl;
-        for (size_t i = 0; i < updated_rates.size(); ++i)
-          cerr << updated_rates[i] << "\t";
-        cerr << endl << "optimized likelihood:\t"
+      if (VERBOSE)
+        cerr << "scaled log-likelihood: "
              << log_likelihood(J, D, updated_rates) << endl;
-      }
-
-      /* scale rates and branches to have unit branch length
-         corresponding to one expected transition per site */
-      const double the_rate_factor = rate_factor(updated_rates);
-      vector<double> scaled_rates = updated_rates;
-      vector<double> scaled_D = D;
-      for (size_t i = 0; i < updated_rates.size(); ++i) {
-        scaled_rates[i] /= the_rate_factor;
-        scaled_D[i] *= the_rate_factor;
-      }
-
-      vector<double> scaled_branches(branches_from_paths);
-      for (size_t b = 0; b < branches_from_paths.size(); ++b)
-        scaled_branches[b] *= the_rate_factor;
-
-      if (VERBOSE) {
-        cerr << "scaled rates:\t" << endl;
-        for (size_t i = 0; i < updated_rates.size(); ++i)
-          cerr << scaled_rates[i] << "\t";
-        cerr << endl;
-        cerr << "scaled branches:\t" << endl;
-        for (size_t i = 0; i < branches.size(); ++i)
-          cerr << scaled_branches[i] << "\t";
-
-        cerr << endl
-             <<"new log-likelihood= "
-             << log_likelihood(J, scaled_D, scaled_rates) << endl;
-      }
     }
-  }
-  catch (std::bad_alloc &ba) {
-    cerr << "ERROR: could not allocate memory" << endl;
-    return EXIT_FAILURE;
   }
   catch (const std::exception &e) {
     cerr << e.what() << endl;
