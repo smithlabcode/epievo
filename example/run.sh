1ebfe2cc771a8fc4074fd75d60cdf816afc96035
jqujqu <jqu@usc.edu>
2018-04-16 16:24:18 -0700

    Modify example commands after input file format changed

--- example/run.sh
+++ example/run.sh
@@ -1,4 +1,7 @@
 prefix=test
-evosim ${prefix}.param -v -p ${prefix}_path.txt -o ${prefix}_meth.txt
-Rscript plot.R ${prefix}
+../src/prog/epievo_sim ${prefix}.param tree.nwk -o ${prefix}.outmeth -n 100000 -p ${prefix}.global_jumps -v 
+../src/prog/global_jumps_to_paths tree.nwk ${prefix}.outmeth ${prefix}.global_jumps ${prefix}.paths
+Rscript plot.R ${prefix} 2 0 5000 
+
+
 
