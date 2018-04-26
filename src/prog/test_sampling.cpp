672e2aa84a589802f75d6cfe246b5c42e498470b
jqujqu <jqu@usc.edu>
2018-04-26 12:22:51 -0700

    Remover obsolete posterior-sampling code

--- src/prog/test_sampling.cpp
+++ src/prog/test_sampling.cpp
@@ -31,12 +31,9 @@
 #include "smithlab_os.hpp"
 #include "PhyloTreePreorder.hpp"
 #include "Path.hpp"  /* related to Path */
-
 #include "EpiEvoModel.hpp" /* model_param */
 #include "TripletPattern.hpp"
-// #include "jump.hpp"
 #include "StateSeq.hpp"
-
 #include "SingleSampler.hpp"
 
 using std::vector;
@@ -68,155 +65,74 @@ append_to_pathfile_local(const string &pathfile, const string &node_name,
 
 
 ////////////////////////////////////////////////////////////////////////////////
+void
+paths_to_states_start(const vector<Path> &path_by_site,
+                      vector<size_t> &states) {
+  states.clear();
+  for (size_t i = 0; i < path_by_site.size(); ++i) {
+    states.push_back(path_by_site[i].init_state ? 1 : 0);
+  }
+  assert(states.size() == path_by_site.size());
+}
 
 void
-propose_new_path(const vector<double> &rates, const Path &l,
-                 const Path &m, const Path &r,
-                 Path &new_m, std::mt19937 &gen) {
-  size_t curr_context = l.init_state*4 + m.init_state*2 + r.init_state;
-  double curr_t = 0;
-  bool curr_state = m.init_state;
-  new_m.init_state = m.init_state;
-  new_m.tot_time = m.tot_time;
-  new_m.jumps.clear();
-  Environment env(l, r);
-  for (size_t i = 1; i < env.breaks.size(); ++i) {
-    curr_context = env.left[i]*4 + curr_state*2 + env.right[i];
-    while (curr_t < env.breaks[i]) {
-      std::exponential_distribution<double> exp_distr(rates[curr_context]);
-      const double holding_time = exp_distr(gen);
-      if (curr_t + holding_time < env.breaks[i]) {
-        curr_t += holding_time;
-        curr_state = !curr_state;
-        curr_context = curr_context ^ 2ul; // flip middle bit
-        new_m.jumps.push_back(curr_t);
-      } else {
-        curr_t = env.breaks[i];
-      }
-    }
+paths_to_states_end(const vector<Path> &path_by_site,
+                    vector<size_t> &states) {
+  states.clear();
+  for (size_t i = 0; i < path_by_site.size(); ++i) {
+    states.push_back((size_t)(path_by_site[i].end_state()));
   }
 }
-
-double
-log_proposal_prob(const vector<double> &rates, const Path &l,
-                  const Path &m, const Path &r) {
-  size_t curr_context = l.init_state*4 + m.init_state*2 + r.init_state;
-  double curr_t = 0;
-  bool curr_state = m.init_state;
-  Environment env(l, r);
-  double log_prob = 0;
-  size_t j = 0;
-  for (size_t i = 1; i < env.breaks.size(); ++i) {
-    while (j < m.jumps.size() && m.jumps[j] < env.breaks[i]) {
-      curr_context = env.left[i]*4 + curr_state*2 + env.right[i];
-      const double holding_time = m.jumps[j] - curr_t;
-      log_prob += log(rates[curr_context]) - rates[curr_context]*holding_time;
-      curr_t += holding_time;
-      ++j;
-      curr_state = !curr_state;
-      curr_context = curr_context ^ 2ul; // flip middle bit
-    }
-    log_prob += -rates[curr_context]*(env.breaks[i] - curr_t);
-    curr_t = env.breaks[i];
-  }
-  return log_prob;
-}
-
-static double
-log_lik_ratio(const vector<double> &rates,
-              const PathContextStat &pcs_num,
-              const PathContextStat &pcs_denom) {
-  double result = 0.0;
-  for (size_t i = 0; i < 8; ++i) {
-    result += (pcs_num.jumps_in_context[i] -
-               pcs_denom.jumps_in_context[i])*log(rates[i]) -
-      (pcs_num.time_in_context[i] -
-       pcs_denom.time_in_context[i]) * rates[i];
-  }
-  return result;
-}
-
-
-double
-log_accept_rate(const vector<double> &rates,
-                const Path &ll, const Path &l, const Path &m,
-                const Path &r, const Path &rr, const Path &new_m) {
-  const double lp_prop_old = log_proposal_prob(rates, l, m, r);
-  const double lp_prop_new = log_proposal_prob(rates, l, new_m, r);
-
-  PathContextStat pcs_old(l, m, r);
-  PathContextStat pcs_new(l, new_m, r);
-
-  PathContextStat pcs_old_l(ll, l, m);
-  PathContextStat pcs_new_l(ll, l, new_m);
-
-  PathContextStat pcs_old_r(m, r, rr);
-  PathContextStat pcs_new_r(new_m, r, rr);
-
-  double lr = lp_prop_old - lp_prop_new +
-    log_lik_ratio(rates, pcs_new, pcs_old) +
-    log_lik_ratio(rates, pcs_new_l, pcs_old_l) +
-    log_lik_ratio(rates, pcs_new_r, pcs_old_r);
-  return lr;
-}
-
 
 static void
-propose_path(vector<std::exponential_distribution<double> > &distrs,
-             std::mt19937 &gen,
-             const EpiEvoModel &the_model,
-             const Path &left, const Path &right, Path &mid) {
+write_states_at_nodes(const string &outfile, const EpiEvoModel &the_model,
+                      const vector<string> &node_names,
+                      const vector<vector<Path> > &all_paths) {
+  std::ofstream out(outfile.c_str());
+  if (!out)
+    throw std::runtime_error("bad output file: " + outfile);
 
-  size_t triplet = triple2idx(left.init_state, mid.init_state, right.init_state);
-  double current_time = 0.0;
+  const size_t n_sequences = all_paths.size();
+  const size_t n_sites = all_paths[1].size();
+  cerr << n_sequences << " nodes and " << n_sites << " sites" << endl;
+  out << '#';
+  copy(node_names.begin(), node_names.end(),
+       std::ostream_iterator<string>(out, "\t"));
+  out << '\n';
 
-  auto i = left.jumps.begin();
-  auto k = right.jumps.begin();
-
-  mid.jumps.clear();
-
-  // assume left and right have their final jump set to the total
-  // time; we can fix this later
-  while (i != left.jumps.end() && k != right.jumps.end()) {
-    const double next_context_change = min(*i, *k);
-
-    // sample a holding time in the current state
-    const double holding = distrs[triplet](gen);
-
-    if (current_time + holding < next_context_change) {
-      current_time += holding;             // update the timepoint
-      mid.jumps.push_back(current_time);   // add the timepoint to mid jumps
-      triplet = flip_mid_bit(triplet);     // update the context
-    }
-    else {
-      current_time = next_context_change;
-      if (*i < *k) {
-        triplet = flip_left_bit(triplet);
-        ++i;
-      }
-      else {
-        triplet = flip_right_bit(triplet);
-        ++k;
-      }
-    }
-    cerr << endl;
+  vector<vector<size_t> > states(n_sequences);
+  paths_to_states_start(all_paths[1], states[0]);
+  for (size_t i = 1; i < n_sequences; ++i) {
+    paths_to_states_end(all_paths[i], states[i]);
   }
+
+  for (size_t i = 0; i < n_sites; ++i) {
+    out << i;
+    for (size_t j = 0; j < n_sequences; ++j)
+      out << '\t' << states[j][i] ;
+    out << '\n';
+  }
+
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, const char **argv) {
   try {
 
     bool VERBOSE = false;
     bool SCALE = true;
-    size_t the_site = 0;
-    string node_name;
     string outfile;
+    string outstatefile;
 
     size_t rounds = 1;
 
     string param_file;
     string tree_file;
-    ////////////////////////////////////////////////////////////////////////
+
+    size_t rng_seed = std::numeric_limits<size_t>::max();
+
+    ///////////////////////////////////////////////////////////////////////////
     OptionParser opt_parse(strip_path(argv[0]), "test triple path",
                            " <paths-file>");
     opt_parse.add_opt("param", 'p', "parameter file",
@@ -225,14 +141,13 @@ int main(int argc, const char **argv) {
                       true, tree_file);
     opt_parse.add_opt("verbose", 'v', "print more run info",
                       false, VERBOSE);
-    opt_parse.add_opt("site", 's', "site to simulate",
-                      true, the_site);
-    opt_parse.add_opt("node", 'n', "name of node below edge to sample",
-                      true, node_name);
+    opt_parse.add_opt("seed", 's', "rng seed", false, rng_seed);
     opt_parse.add_opt("rounds", 'r', "number of posterior update cycles",
                       true, rounds);
-    opt_parse.add_opt("outfile", 'o', "output file for posterior-updated paths",
+    opt_parse.add_opt("outfile", 'o', "outfile for posterior-updated paths",
                       true, outfile);
+    opt_parse.add_opt("outstatefile", 'S', "outfile for states at nodes",
+                      false, outstatefile);
     vector<string> leftover_args;
     opt_parse.parse(argc, argv, leftover_args);
     if (argc == 1 || opt_parse.help_requested()) {
@@ -248,12 +163,12 @@ int main(int argc, const char **argv) {
       cerr << opt_parse.option_missing_message() << endl;
       return EXIT_SUCCESS;
     }
-    if (leftover_args.size() != 2) {
+    if (leftover_args.size() != 1) {
       cerr << opt_parse.help_message() << endl;
       return EXIT_SUCCESS;
     }
     const string pathsfile(leftover_args.front());
-    ////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
 
     if (VERBOSE)
       cerr << "[READING PATHS: " << pathsfile << "]" << endl;
@@ -270,60 +185,28 @@ int main(int argc, const char **argv) {
            << "n_sites=" << n_sites << endl;
 
     if (VERBOSE)
-      cerr << "[READING PARAMETER FILE: "
-           << param_file << ", " << tree_file << "]" << endl;
+      cerr << "[READING PARAMETER FILE: " << param_file << ", "
+           << tree_file << "]" << endl;
+
     EpiEvoModel the_model;
     read_model(SCALE, param_file, tree_file, the_model);
+
     if (VERBOSE)
       cerr << the_model << endl;
 
-    /* standard mersenne_twister_engine seeded with rd()*/
-    std::random_device rd;
-    std::mt19937 gen(rd());
-
-    // get the id for the desired node
-    vector<string>::const_iterator name_idx =
-      find(begin(node_names), end(node_names), node_name);
-    if (name_idx == node_names.end())
-      throw runtime_error("invalid node name: " + node_name);
-
-    const size_t node_id = name_idx - node_names.begin();
-    const size_t parent_id = the_model.parent_ids[node_id];
-    const double branch_length = the_model.branches[node_id];
-
-    if (VERBOSE) {
-      cerr << "node name: " << node_name << endl
-           << "node id: " << node_id << endl
-           << "parent name: " << node_names[parent_id] << endl
-           << "parent id: " << parent_id << endl
-           << "branch length: " << branch_length << endl
-           << "site: " << the_site << endl
-           << "total jumps: "
-           << all_paths[node_id][the_site].jumps.size() << endl;
+    /* standard mersenne_twister_engine seeded with rd() */
+    if (rng_seed == std::numeric_limits<size_t>::max()) {
+      std::random_device rd;
+      rng_seed = rd();
     }
 
-    vector<std::exponential_distribution<double> > distrs;
-    for (size_t i = 0; i < the_model.triplet_rates.size(); ++i) {
-      auto ed(std::exponential_distribution<double>(the_model.triplet_rates[i]));
-      cout << bitset<3>(i) << '\t' << ed << endl;
-      distrs.push_back(ed);
-    }
+    if (VERBOSE)
+      cerr << "rng seed: " << rng_seed << endl;
 
-    Path left_path(all_paths[node_id][the_site - 1]);
-    left_path.jumps.push_back(left_path.tot_time);
+    std::mt19937 gen(rng_seed);
 
-    Path right_path(all_paths[node_id][the_site + 1]);
-    right_path.jumps.push_back(right_path.tot_time);
-
-    Path the_path(all_paths[node_id][the_site]);
-    propose_path(distrs, gen, the_model, left_path, right_path, the_path);
-
-    cout << all_paths[node_id][the_site].end_state() << endl;
-    cout << the_path.end_state() << endl;
-
-
-    ////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
     cerr << "-- TEST homogeneous interval end-cond. sampling BELOW --" << endl;
     double T = 0.5;
     size_t a = 1;
@@ -333,42 +216,64 @@ int main(int argc, const char **argv) {
          << "       start state     \t " << a << endl
          << "       end state       \t " << b << endl;
 
-
     // prepare helper values
-    vector<double> rates;
+    vector<double> rates(2, 0.0);
+    rates[0] = the_model.triplet_rates[0]; // 000
+    rates[1] = the_model.triplet_rates[2]; // 010
     vector<double> eigen_vals;
     vector<vector<double> > U;
     vector<vector<double> > Uinv;
-    decompose(the_model.triplet_rates[0],
-              the_model.triplet_rates[2],
-              rates, eigen_vals, U, Uinv);
+
+    decompose(rates, eigen_vals, U, Uinv);
 
     vector<double> jump_times;
     end_cond_sample(rates, eigen_vals, U, Uinv,
                     a, b, T, gen, jump_times);
-
-
 
     cerr << " sampled jump times: " << endl;
     for (size_t i = 0; i < jump_times.size(); ++i)
       cerr << jump_times[i] << ",\t" ;
     cerr << endl << "total " << jump_times.size()-2 << " jumps" << endl;
 
+    //////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+    cerr << "----- TEST upward downward sampling BELOW ---------" << endl;
 
-    ////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////
-    cerr << "---------- TEST upward downward sampling BELOW -----------" << endl;
-    //const size_t rounds = 10;
+    /*
+      size_t site = 10;
+      write_root_to_pathfile_local(outfile, the_model.node_names.front());
+      for (size_t i = 0; i < 10000000; ++i) {
+      vector<Path> new_path;
+      gibbs_site(the_model, site, all_paths, gen, new_path);
+
+      std::ofstream outpath(outfile.c_str(), std::ofstream::app);
+      for (size_t node_id = 4; node_id < n_nodes; ++node_id) {
+      if (new_path[node_id].jumps.size() > 0)
+      outpath << "NODE:" << the_model.node_names[node_id] << ";" << i << '\t'
+      << site << '\t' << new_path[node_id] << '\n';
+      }
+      }
+    */
+
 
     for (size_t k = 0; k < rounds; ++k) {
       cerr << "pass " << k+1 << endl;
-      for (size_t i = 1; i < n_sites-1; ++i) {
+      for (size_t i = 1; i < n_sites - 1; ++i) {
+        std::uniform_int_distribution<> dis(1, n_sites - 2);
+        size_t site = dis(gen);
         vector<Path> new_path;
-        gibbs_site(the_model, i, all_paths, gen, new_path);
+        gibbs_site(the_model, site, all_paths, gen, new_path);
         for (size_t node_id = 1; node_id < n_nodes; ++node_id)
-          all_paths[node_id][i] = new_path[node_id];
+          all_paths[node_id][site] = new_path[node_id];
+      }
+
+      if (!outstatefile.empty()) {
+        const string m_outstatefile = outstatefile + std::to_string(k);
+        write_states_at_nodes(m_outstatefile, the_model,
+                              node_names, all_paths);
       }
     }
+    cerr << endl;
 
     write_root_to_pathfile_local(outfile, the_model.node_names.front());
 
@@ -377,13 +282,6 @@ int main(int argc, const char **argv) {
                                all_paths[node_id]);
     }
 
-    // cerr << "[old_path]" << endl;
-    // for (size_t i = 1; i < new_path.size(); ++i)
-    //   cerr << "node_"<< i << ":\t" << all_paths[i][the_site] << endl;
-
-    // cerr << "[new_path]" << endl;
-    // for (size_t i = 1; i < new_path.size(); ++i)
-    //   cerr << "node_"<< i << ":\t" << new_path[i] << endl;
   }
   catch (const std::exception &e) {
     cerr << e.what() << endl;
