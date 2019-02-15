03905467a498e3a777cc9c2f59661b4f59992d01
lizji1992 <lizji1992@gmail.com>
2019-02-15 13:05:02 -0800

    Added options to specify initial parameter and tree.

--- test/est_params.sh
+++ test/est_params.sh
@@ -1,7 +1,8 @@
 num=50
 sites=10000
 
-outPrefix=test
+estRatesTree=0
+outPrefix=tree
 
 ################################################################################
 ####### ##    ###   ##    ###    ##   ###    #    ##    ###   ###    ##
@@ -19,26 +20,37 @@ mkdir -p $figDir
 
 ################################################################################
 
-paramFile=$inDir/$outPrefix.param
-treeFile=$inDir/$outPrefix.nwk
+trueParamFile=$inDir/$outPrefix.param
+trueTreeFile=$inDir/$outPrefix.nwk
+initParamFile=$trueParamFile
+initTreeFile=$trueTreeFile
+
 
 ################################################################################
 
 print_usage() {
   printf "Usage: $(basename $0)
           [-n number of samples (50) ] [-s sites (10000)]
-          [-f output file prefix (test)]
-          [-p parameter file (input/test.param)]
-          [-t tree file (input/test.nwk)]\n"
+          [-f output file prefix (tree)]
+          [-T estimate both rates and branch lengths]
+          [-p parameter file (input/tree.param)]
+          [-t tree file (input/tree.nwk)]
+          [-i initial parameter file (input/tree.param)]
+          [-j initial tree (input/tree.nwk)]\n"
 }
 
-while getopts 'n:s:f:p:t:h' flag; do
+while getopts 'n:s:f:Tp:t:i:j:h' flag; do
   case "${flag}" in
     n) num="${OPTARG}" ;;
     s) sites="${OPTARG}" ;;
     f) outPrefix="${OPTARG}" ;;
-    p) paramFile="${OPTARG}" ;;
-    t) treeFile="${OPTARG}" ;;
+    T) estRatesTree=1 ;; 
+    p) trueParamFile="${OPTARG}"
+       initParamFile=$trueParamFile ;;
+    t) trueTreeFile="${OPTARG}"
+       initTreeFile=$trueTreeFile ;;
+    i) initParamFile="${OPTARG}" ;;
+    j) initTreeFile="${OPTARG}" ;;
     h) print_usage
        exit 0 ;;
     *) print_usage
@@ -55,12 +67,16 @@ done
 > $outDir/${outPrefix}_rt1.stats
 
 for (( i=0; i<$num; i++ )); do
-  epievo_sim $paramFile -t $treeFile -n $sites \
+  epievo_sim $trueParamFile -t $trueTreeFile -n $sites \
     -o $outDir/$outPrefix.outmeth -p $outDir/$outPrefix.global_jumps
-  global_jumps_to_paths $treeFile $outDir/$outPrefix.outmeth \
+  global_jumps_to_paths $trueTreeFile $outDir/$outPrefix.outmeth \
     $outDir/$outPrefix.global_jumps $outDir/$outPrefix.path_local
-  epievo_est_complete -S -p $paramFile -t $treeFile \
-    -o $outDir/$outPrefix.param.update $outDir/$outPrefix.path_local
+  mleCMD="epievo_est_complete -S -p $initParamFile -t $initTreeFile -v
+-o $outDir/$outPrefix.param.update $outDir/$outPrefix.path_local"
+  if [ "$estRatesTree" -eq 1 ]; then
+      mleCMD="${mleCMD} -b"
+  fi
+  eval $mleCMD
  
   updatedParamFile=$outDir/$outPrefix.param.update
   text=$(<$updatedParamFile)
@@ -80,5 +96,5 @@ for (( i=0; i<$num; i++ )); do
 
 done
 
-Rscript ../rscripts/compare_params.R -t $paramFile -e $outDir/$outPrefix \
+Rscript ../rscripts/compare_params.R -t $trueParamFile -e $outDir/$outPrefix \
   -o $figDir/${outPrefix}_N$sites.pdf
