a6e6610638f4d66b4f1ec6f361020fdfaa3361ca
xjlizji <lizji1992@gmail.com>
2019-09-04 17:44:42 -0700

    Plot histograms instad of density plots for D

--- rscripts/plot_sufficient_stats.R
+++ rscripts/plot_sufficient_stats.R
@@ -41,11 +41,11 @@ output <- opt$options$output
 #din <- "~/codes/epievo/test/output/mcmc"
 #output <- "~/codes/epievo/test/output/mcmc/test.pdf"
 
-files.forward <- list.files(path=din, pattern=paste0(prefix, ".*.forward$"))
+files.forward <- list.files(path=din, pattern=paste0(prefix, ".forward$"))
 nodes <- sapply(strsplit(files.forward, ".", fixed=T), function(x) x[2])
 names(files.forward) <- nodes
 
-files.mcmc <- list.files(path=din, pattern=paste0(prefix, ".*.mcmc$"))
+files.mcmc <- list.files(path=din, pattern=paste0(prefix, ".mcmc$"))
 names(files.mcmc) <- sapply(strsplit(files.mcmc, ".", fixed=T), function(x) x[2])
 
 pdf(file=output, width=6.7, height=6.7)
@@ -55,46 +55,39 @@ par(mgp = c(0.5, 0, 0))
 par(tcl = -0.17)
 par(cex.lab=0.8, cex.axis=0.8, cex.main=1)
 
-for (node in nodes) {
-  node <- "leaf"
-  dat.forward <- read.table(file.path(din, files.forward[node]), header=T)
-  dat.mcmc <- read.table(file.path(din, files.mcmc[node]), header=T)
-  
-  itrs <- unique(dat.mcmc$ITR)
-  batch <- nrow(dat.forward)
-  stats.name <- colnames(dat.forward)[3:ncol(dat.forward)]
+node <- 1
+dat.forward <- read.table(file.path(din, files.forward[node]), header=T)
+dat.mcmc <- read.table(file.path(din, files.mcmc[node]), header=T)
 
-  for (stat in stats.name) {
-    # plot batch mean over iterations
-    mean.forward <- mean(dat.forward[, stat])
-    mean.mcmc <- sapply(itrs,
-                        function (x) mean(dat.mcmc[dat.mcmc$ITR==x, stat]))
-    
-    plot(mean.mcmc, type="l", xlab="itr", ylab=stat, main=stat,
-         ylim=range(mean.forward, mean.mcmc))
-    abline(h=mean.forward, lty=2, col="red")
-    
-    # plot final distributions
-    samples.forward <- dat.forward[, stat]
-    samples.mcmc <- dat.mcmc[(nrow(dat.mcmc)-batch+1):nrow(dat.mcmc), stat]
-    if (substr(stat, 1, 1) == "J") {
-      max_jumps <- max(samples.forward, samples.mcmc)
-      hist(samples.forward, col=rgb(1,0,0,0.5), xlim=c(0, max_jumps),
-           breaks=seq(0-0.5, max_jumps+0.5, by=1), ylim=c(0, batch),
-           main=stat, xlab=stat)
-      hist(samples.mcmc, col=rgb(0,0,1,0.5), add=T,
-           breaks=seq(0-0.5, max_jumps+0.5, by=1))
-      legend("topright", c("true", "sampled"),
-             bty="n", fill=c(rgb(1,0,0,0.5), rgb(0,0,1,0.5)))
-    } else {
-      d1 <- density(samples.forward)
-      d2 <- density(samples.mcmc)
-      maxD <- max(max(d1$y), max(d2$y))
-      plot(d1, col=rgb(1,0,0,0.5), main=stat, ylim=c(0, maxD), xlab=stat)
-      lines(d2, col=rgb(0,0,1,0.5))
-      legend("topright", c("true", "sampled"),
-             bty="n", fill=c(rgb(1,0,0,0.5), rgb(0,0,1,0.5)))
-    }
+itrs <- unique(dat.mcmc$ITR)
+batch <- nrow(dat.forward)
+stats.name <- colnames(dat.forward)[3:ncol(dat.forward)]
+
+for (stat in stats.name) {
+  # plot batch mean over iterations
+  mean.forward <- mean(dat.forward[, stat])
+  mean.mcmc <- sapply(itrs,
+                      function (x) mean(dat.mcmc[dat.mcmc$ITR==x, stat]))
+  
+  # plot(mean.mcmc, type="l", xlab="itr", ylab=stat, main=stat,
+  #      ylim=range(mean.forward, mean.mcmc))
+  # abline(h=mean.forward, lty=2, col="red")
+  
+  # plot final distributions
+  samples.forward <- dat.forward[, stat]
+  samples.mcmc <- dat.mcmc[(nrow(dat.mcmc)-batch+1):nrow(dat.mcmc), stat]
+  if (substr(stat, 1, 1) == "J") {
+    max_jumps <- max(samples.forward, samples.mcmc)
+    hist(samples.forward, col=rgb(1,0,0,0.5), xlim=c(0, max_jumps),
+         breaks=seq(0-0.5, max_jumps+0.5, by=1), ylim=c(0, batch),
+         main=stat, xlab=stat)
+    hist(samples.mcmc, col=rgb(0,0,1,0.5), add=T,
+         breaks=seq(0-0.5, max_jumps+0.5, by=1))
+    legend("topright", c("true", "sampled"),
+           bty="n", fill=c(rgb(1,0,0,0.5), rgb(0,0,1,0.5)))
+  } else {
+    hist(samples.forward, col=rgb(1,0,0,0.5), main=paste0(stat, ".forward"), xlab=stat)
+    hist(samples.mcmc, col=rgb(0,0,1,0.5), main=paste0(stat, ".mcmc"), xlab=stat)
   }
 }
 
