d6ffdc57546c0fb84da1286fb1ba8ac3cc6f2a6c
Andrew D Smith <andrewds@usc.edu>
2020-03-02 16:34:37 -0800

    cleaning up average_paths

--- src/prog/average_paths.cpp
+++ src/prog/average_paths.cpp
@@ -43,12 +43,12 @@ using std::runtime_error;
 using std::numeric_limits;
 using std::to_string;
 
-///////////////////////////////////////////////////////////////////////////
+
 static void
 add_paths(const vector<vector<Path> > &paths,
           vector<vector<double> > &average_paths) {
   const size_t n_points = average_paths[0].size();
-  const double bin = paths[1][0].tot_time / (n_points - 1);
+  const double bin = paths[1][0].tot_time/(n_points - 1);
   for (size_t site_id = 0; site_id < paths[1].size(); site_id++) {
     size_t prev_state = paths[1][site_id].init_state;
     average_paths[site_id][0] += prev_state;
@@ -64,49 +64,34 @@ add_paths(const vector<vector<Path> > &paths,
 
 static void
 write_output(const string &outfile, const vector<vector<double> >  &states) {
-  std::ofstream of;
-  if (!outfile.empty()) of.open(outfile.c_str());
-  std::ostream out(outfile.empty() ? std::cout.rdbuf() : of.rdbuf());
+  std::ofstream out(outfile);
   if (!out)
-    throw std::runtime_error("bad output file: " + outfile);
-  
+    throw runtime_error("bad output file: " + outfile);
+
   const size_t n_sites = states.size();
-  
-  for (size_t site_id = 0; site_id < n_sites; site_id++) {
+  for (size_t site_id = 0; site_id < n_sites; ++site_id) {
     out << states[site_id].front();
     for (size_t t = 1; t < states[site_id].size(); t++)
       out << "\t" << states[site_id][t];
     out << endl;
   }
-
 }
 
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, const char **argv) {
   try {
     bool VERBOSE = false;
 
     string outfile;
-    
     size_t n_points = 100;
-    
-    ///////////////////////////////////////////////////////////////////////////
-    OptionParser opt_parse(strip_path(argv[0]),
-                           "Average local paths", "<input directory>");
-    opt_parse.add_opt("outfile", 'o',
-                      "output file of averaged states(default: stdout)",
-                      false, outfile);
-    opt_parse.add_opt("npoints", 'n',
-                      "number of equally spaced bins",
-                      false, n_points);
-    opt_parse.add_opt("verbose", 'v', "print more run info",
-                      false, VERBOSE);
 
+    ///////////////////////////////////////////////////////////////////////////
+    OptionParser opt_parse(strip_path(argv[0]), "average local paths",
+                           "<input directory>");
+    opt_parse.add_opt("outfile", 'o', "output file", true, outfile);
+    opt_parse.add_opt("npoints", 'n', "number of bins", false, n_points);
+    opt_parse.add_opt("verbose", 'v', "print more run info", false, VERBOSE);
+    opt_parse.set_show_defaults();
     vector<string> leftover_args;
     opt_parse.parse(argc, argv, leftover_args);
     if (argc == 1 || opt_parse.help_requested()) {
@@ -128,41 +113,38 @@ int main(int argc, const char **argv) {
     }
     const string paths_dir(leftover_args[0]);
     ///////////////////////////////////////////////////////////////////////////
-    /* (1) READING PATHS FROM FILE */
+
     if (VERBOSE)
-      cerr << "[READING PATHS FROM: " << paths_dir << endl;
+      cerr << "[READING PATHS FROM: " << paths_dir << "]" << endl;
     vector<string> files;
     read_dir(paths_dir, "local_paths", files);
-
+    const size_t n_files = files.size();
 
     vector<vector<Path> > paths; // along multiple branches
     vector<string> node_names;
     read_paths(files[0], node_names, paths);
-    
+
     const size_t n_sites = paths[1].size();
-    vector<vector<double> > average_paths =
-        vector<vector<double> > (n_sites, vector<double> (n_points, 0.0));
+    vector<vector<double> > average_paths(n_sites, vector<double>(n_points, 0));
     add_paths(paths, average_paths);
 
-    for (size_t i = 1; i < files.size(); i++) {
+    for (size_t i = 1; i < n_files; i++) {
       paths.clear(); // along multiple branches
       vector<string> node_names;
       read_paths(files[i], node_names, paths);
       add_paths(paths, average_paths);
     }
-    /* AVERAGING PATHS */
-    for (size_t site_id = 0; site_id < average_paths.size(); site_id++)
-      transform(average_paths[site_id].begin(), average_paths[site_id].end(),
-                average_paths[site_id].begin(),
-                std::bind(std::divides<double>(), std::placeholders::_1,
-                          files.size()));
 
-    /* (2) OUTPUT */
+    /* AVERAGING PATHS */
+    for (size_t site_id = 0; site_id < average_paths.size(); ++site_id)
+      transform(begin(average_paths[site_id]), end(average_paths[site_id]),
+                begin(average_paths[site_id]),
+                [&](const double x) {return x/n_files;});
+
     if (VERBOSE)
-      cerr << "[WRITING OUTPUT]" << endl;
+      cerr << "[WRITING OUTPUT TO: ]" << outfile << "]" << endl;
     write_output(outfile, average_paths);
   }
-
   catch (const std::exception &e) {
     cerr << e.what() << endl;
     return EXIT_FAILURE;
